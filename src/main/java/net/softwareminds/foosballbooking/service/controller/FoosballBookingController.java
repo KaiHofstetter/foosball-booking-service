68f866e2fba4487070e2cc649a2820cf4607d2af
Kai <kai.hofstetter@gmx.de>
2014-10-05 07:49:51 +0200

    Resource path changed to bookings.

--- src/main/java/net/softwareminds/foosballbooking/service/controller/FoosballBookingController.java
+++ src/main/java/net/softwareminds/foosballbooking/service/controller/FoosballBookingController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/booking")
+@RequestMapping("/bookings")
 public class FoosballBookingController {
 
   private final BookingStorage storage;
