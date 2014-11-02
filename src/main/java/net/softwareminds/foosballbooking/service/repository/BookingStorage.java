7239ed6dcc22c6d7a04665382562c07461e933dc
Kai <kai.hofstetter@gmx.de>
2014-11-02 07:46:21 +0100

    The booking id is now a String, which is easier to handle.

--- src/main/java/net/softwareminds/foosballbooking/service/repository/BookingStorage.java
+++ src/main/java/net/softwareminds/foosballbooking/service/repository/BookingStorage.java
@@ -3,10 +3,12 @@ package net.softwareminds.foosballbooking.service.repository;
 import net.softwareminds.foosballbooking.service.domain.Booking;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface BookingStorage {
   public Booking storeBooking(Booking booking);
-  public void deleteBooking(Booking booking);
-  public Collection<Booking> getAllBookings();
+  public void deleteBooking(String bookingId);
+  public Booking getBooking(String bookingId);
+  public List<Booking> getAllBookings();
 }
