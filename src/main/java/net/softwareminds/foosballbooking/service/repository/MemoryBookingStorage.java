618df7e7d6318ade5306b4173ea70e277e93daa2
KaiHofstetter <kai.hofstetter@gmx.de>
2014-11-08 22:33:36 +0100

    MemoryBookingStorage sorts the booking by begin.

--- src/main/java/net/softwareminds/foosballbooking/service/repository/MemoryBookingStorage.java
+++ src/main/java/net/softwareminds/foosballbooking/service/repository/MemoryBookingStorage.java
@@ -3,6 +3,7 @@ package net.softwareminds.foosballbooking.service.repository;
 import net.softwareminds.foosballbooking.service.domain.Booking;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -32,6 +33,9 @@ public class MemoryBookingStorage implements BookingStorage {
 
   @Override
   public List<Booking> getAllBookings() {
-    return new ArrayList(store.values());
+    List<Booking> bookingList = new ArrayList(store.values());
+    Collections.sort(bookingList, (Booking o1, Booking o2) -> o1.getBegin().compareTo(o2.getBegin()));
+
+    return bookingList;
   }
 }
